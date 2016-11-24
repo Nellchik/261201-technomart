5182cf319f90a4973a97d12952574e7aba24cecc
Nellchik <nelly.saifutdinova@gmail.com>
2016-11-25 00:52:54 +0700

    slider

--- js/script.js
+++ js/script.js
@@ -1,42 +1,108 @@
 var link = document.querySelector(".write-us");
-var popup = document.querySelector(".feedback");
+var surface = document.querySelector(".feedback");
 var close = document.querySelector(".cross-close-feedback");
+
+var form = surface.querySelector("form");
+var guest = surface.querySelector("[name=name]");
+var email = surface.querySelector("[name=e-mail]");
+
+
 var map = document.querySelector(".map-small");
 var bigmap = document.querySelector(".map-big");
 var closemap = document.querySelector(".cross-close-map");
-
-var buy = document.querySelectorAll(".action .buy");
+var buyBtns = document.querySelectorAll(".action .buy");
 var incart = document.querySelector(".goods-in-cart");
 var closeincart = document.querySelector(".cross-close-cart");
 
-link.addEventListener("click", function (event) {
+var controls = document.querySelector(".slide-switch");    
+var hideslide1 = document.querySelector(".show");
+var slide1 = document.querySelector(".slide-1");
+var slide2 = document.querySelector(".slide-2");
+
+    
+controls.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.add("feedback-show");
-  console.log("Клик по кнопке Заблудились? Напишите нам!");
-});
-close.addEventListener("click", function (event) {
-  event.preventDefault();
-  popup.classList.remove("feedback-show");
-  console.log("Клик по крестику закрытия формы")
-});
-map.addEventListener("click", function (event) {
-  event.preventDefault();
-  bigmap.classList.add("map-big-show");
-  console.log("клик по маленькой карте")
-});
-closemap.addEventListener("click", function (event) {
-  event.preventDefault();
-  bigmap.classList.remove("map-big-show");
-  console.log("Клик по крестику закрытия карты")
-});
-buy.addEventListener("click", function (event) {
-  event.preventDefault();
-  incart.classList.add("goods-in-cart-show");
-  console.log("Клик по кнопке Купить");
+  slide1.classList.remove("slide-1-show");
+  slide1.classList.add("slide-1");
+  slide2.classList.remove("slide-2");
+  slide2.classList.add("slide-2-show"); 
+  console.log("Переключение на 2Слайд");
 });
 
-closeincart.addEventListener("click", function (event) {
+if (link) {
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    surface.classList.add("feedback-show");
+    guest.focus();
+    console.log("Клик по кнопке Заблудились? Напишите нам!");
+    
+  });
+}
+
+
+if (close) {
+  close.addEventListener("click", function(event) {
+    event.preventDefault();
+    surface.classList.remove("feedback-show");
+    surface.classList.remove("feedback-error");
+    console.log("Клик по крестику закрытия формы");
+  });
+}
+
+if (form) {
+  form.addEventListener("submit", function(event) {
+  if (!guest.value || !email.value) {
   event.preventDefault();
-  bigmap.classList.remove("goods-in-cart-show");
-  console.log("Клик по крестику закрытия формы")
-      
+  console.log("Нужно ввести имя и почту");
+    surface.classList.add("feedback-error");
+  } else {
+    localStorage.setItem("guest", guest.value);
+  }
+});
+}
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (surface.classList.contains("feedback-show")) {
+      surface.classList.remove("feedback-show");
+    }    
+  }
+})
+
+if (map) {
+  map.addEventListener("click", function(event) {
+    event.preventDefault();
+    bigmap.classList.add("map-big-show");
+    console.log("клик по маленькой карте");
+  });
+}
+if (closemap) {
+  closemap.addEventListener("click", function(event) {
+    event.preventDefault();
+    bigmap.classList.remove("map-big-show");
+    console.log("Клик по крестику закрытия карты");
+  });
+}
+if (closeincart) {
+  closeincart.addEventListener("click", function(event) {
+  event.preventDefault();
+  incart.classList.remove("goods-in-cart-show");
+  console.log("Клик по крестику закрытия формы");
+});
+}
+
+for (var i = 0; i < buyBtns.length; i++) {
+  buyBtns[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    incart.classList.add("goods-in-cart-show");
+    console.log("Клик по кнопке Купить");
+  });
+}
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (incart.classList.contains("goods-in-cart-show")) {
+      incart.classList.remove("goods-in-cart-show");
+    }    
+  }
+})
